ae794f581fbfcbdd371a67870e2a15a8957aca45
EidorianAvi <reneavila1993@gmail.com>
2020-05-27 11:41:30 -0600

    Build main menu method. ADD to CLI class.

--- lib/models/command_line_interface.rb
+++ lib/models/command_line_interface.rb
@@ -1,9 +1,47 @@
-class CommandLineInterface
+class CommandLineInterface 
   $prompt = TTY::Prompt.new
+  
+  attr_accessor :user
+
 
   def greet
-    puts "Welcome to On Cloud Wine! Please enter your name."
-    user_name = $prompt.ask('Name:', default: ENV["User"])
-    user = User.find_or_create_by(name: user_name)
+    Ascii.greeting_art
+
+    puts "                        Welcome to On Cloud Wine! Please enter your name."
   end
+
+
+  def run
+    greet
+    user_name = $prompt.ask('                   Name:', default: ENV["User"],)
+    @user = User.find_by(name: user_name)
+    system('clear')
+  end
+
+  def main_menu
+    menu_selections = {
+      "Browse Wines" => 1,
+      "View Personal Collection" => 2,
+      "Help" => 3,
+      "Exit" => 4
+    }
+    menu_choice = $prompt.select('Choose an option from the menu below:', menu_selections)
+
+    case menu_choice
+    when 1 
+      Wine.browse_wines
+    when 2
+      User.personal_collection
+    when 3
+      self.help
+    when 4
+      return
+    end
+  end
+
+
+
+
+
 end
+
--- lib/models/wine.rb
+++ lib/models/wine.rb
@@ -1,4 +1,10 @@
 class Wine < ActiveRecord::Base
   has_many :user_wines
   has_many :users, through: :user_wines
+
+  def self.browse_wines
+
+  end
+
+  
 end
