220ec537f96bddfad53837556b6a9708305fd343
github username <github email address>
2020-05-28 11:25:47 -0600

    Create menu navigation and user prompts for drink method

--- lib/models/ascii..rb
+++ lib/models/ascii..rb
@@ -35,7 +35,8 @@ class Ascii
     )( |HHHHHHHH H|   )(
     )( |HHHHHHHH H|   )(
    .)(.|HHHHHHHH H|  .)(.
-  ~~~~~~~~~~~~~~~~  ~~~~~~'
+  ~~~~~~~~~~~~~~~~  ~~~~~~
+   '
     end
 
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -16,17 +16,51 @@ class User < ActiveRecord::Base
     UserWine.where(user_id: self.id, wine_id: wine.id).sum(:count)
   end
 
-  def drink wine_id
-    wine_record = UserWine.where(user_id: self.id, wine_id: wine_id).first
-    if wine_record
-      wine_record[:count] -= 1
-      if wine_record[:count] == 0
-        UserWine.delete(wine_record[:id])
+  def drink
+    if has_wines?
+      wine_id = $prompt.ask('Please enter the ID of the wine you would like to uncork: '.light_green)
+      wine_record = UserWine.where(user_id: self.id, wine_id: wine_id).first
+      if wine_record
+        wine_record[:count] -= 1
+        if wine_record[:count] == 0
+          UserWine.delete(wine_record[:id])
+        else
+          wine_record.save
+        end
+        system('clear')
+        Ascii.bottle_and_glasses
+        puts "Enjoy your bottle of #{wine_record.wine.name}!".light_green
+        puts
+        choices = {"View personal collection" => 1, "Main menu" => 2}
+        selection = $prompt.select("Would you like to return to your personal collection or the main menu?", choices)
+        case selection
+        when 1
+          personal_collection
+        when 2
+          CommandLineInterface.main_menu
+        end
       else
-        wine_record.save
+        puts "Please select a wine you have in your collection".light_red
+        drink
       end
     else
-      puts "Please select a wine you have in your collection"
+      puts "Your collection is currently empty. Please add wine to your collection before trying to drink.".red
+      puts
+      choices = {"Return to main menu" => 1, "Browse wines" => 2}
+      selection = $prompt.select("Would you like to return to the main menu or browse wines?", choices)
+      if selection == 1
+        CommandLineInterface.main_menu
+      else
+        Wine.browse_wines
+      end
+    end
+  end
+
+  def has_wines?
+    if wines.first
+      true
+    else
+      false
     end
   end
 end
